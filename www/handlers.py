e885ee9cf9747c980dbfee0952b16c28cdbf9e78
liuyzh <liuyzh@tju.edu.cn>
2017-01-04 09:29:03 +0800

    day5 finished

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,17 @@
 __author__ = 'Liuyzh'
 
 ' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def indec(request):
+    users = await User.findAll()
+    return{
+        '__template__': 'test.html',
+        'user': users
+    }
